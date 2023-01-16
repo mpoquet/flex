1fbf8af812ef013a38f1bd3027f39a436c37ec05
Millian Poquet <millian.poquet@irit.fr>
2023-01-16 19:48:37 +0100

    template: comment input definition in batsim-expe

--- templates/batsim-expe/flake.nix
+++ templates/batsim-expe/flake.nix
@@ -6,8 +6,25 @@
       inputs.nixpkgs.follows = "nixpkgs"; # tell kapack to use the nixpkgs that is defined above
     };
     flake-utils.url = "github:numtide/flake-utils";
+
+    # define your own batsim and batsched source code as inputs, which are git repository fetched via ssh, using the default branch of each repository.
     mybatsim-src.url = "git+ssh://git@framagit.org/batsim/batsim";
     mybatsched-src.url = "git+ssh://git@framagit.org/batsim/batsched";
+
+    # the lines below show other ways to define inputs depending on your need, taking batsched as an example.
+
+    # batsched, from a git repository fetched via ssh, on a specific branch.
+    # the version used by nix can be updated with this command: nix flake lock --update-input mybatsched-src
+    # mybatsched-src.url = "git+ssh://git@framagit.org/batsim/batsched?ref=master";
+
+    # batsched, from a git repository fetched via ssh, on a specific commit.
+    # mybatsched-src.url = "git+ssh://git@framagit.org/batsim/batsched?rev=203c79745b0718d28fe08a6e2ba863ee9dff0922"; # important: rev, not ref!
+
+    # batsched, from a git repository fetched via https.
+    # mybatsched-src.url = "git+https://git@framagit.org/batsim/batsched";
+
+    # batsched, from a local git repository. This is very useful for private repositories.
+    # mybatsched-src.url = "/home/mpoquet/proj/batsched"; # use the path to the git repository's root
   };
 
   outputs = { self, nixpkgs, nur-kapack, flake-utils, mybatsim-src, mybatsched-src }:
